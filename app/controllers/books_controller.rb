aeb94300eca857c715fc5176de6cc14c28964c3c
Kishino Yuki <yktennis1124@gmail.com>
2024-07-18 01:56:21 +0000

    [finish] book indexページの枠組み

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,4 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
-  end
 
   def create
     @book = Book.new(book_params)
@@ -10,6 +7,7 @@ class BooksController < ApplicationController
     redirect_to book_path(book.id)
   end
   def index
+    @book = Book.new
     @books = Book.all
   end
 
