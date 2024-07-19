5fa23012ba158ceb329d336df3ce32d7c4faef8a
Kishino Yuki <yktennis1124@gmail.com>
2024-07-19 14:45:24 +0000

    [start] user indexページの作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,5 @@
 class BooksController < ApplicationController
-
+  
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
@@ -7,15 +7,30 @@ class BooksController < ApplicationController
     redirect_to book_path(@book.id)
   end
   def index
-    @book = Book.new
+    @new_book = Book.new
     @books = Book.all
   end
 
   def show
+    @new_book = Book.new
     @book = Book.find(params[:id])
+    
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
   
   private
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,18 +6,21 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @books = @user.books
-    @book = Book.new
+    @new_book = Book.new
   end
   
 
   def edit
     @user = User.find(params[:id])
+   unless @user.id == current_user.id
+      redirect_to user_path(@user.id)
+   end
   end
-  
+   
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
-    redirect_to user_path(@user)
+    redirect_to user_path(@user.id)
   end
   
   private
