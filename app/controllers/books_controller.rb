062ed6c9c928d704300deabd43a364f9465a1b0d
Kishino Yuki <yktennis1124@gmail.com>
2024-07-22 07:41:21 +0000

    [finish] テスト前の記述

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,7 +5,7 @@ class BooksController < ApplicationController
     @new_book.user_id = current_user.id
     if @new_book.save
      flash[:notice] = "You have created book successfully."
-     redirect_to book_path(@book.id)
+     redirect_to book_path(@new_book.id)
     else
       @books = Book.all
       render :index
