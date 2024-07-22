8ea9bfa2c8d54763af7edaaa4c69f03a0caedc4b
Kishino Yuki <yktennis1124@gmail.com>
2024-07-22 06:49:12 +0000

    [finish] バリデーション

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,17 +1,19 @@
 class BooksController < ApplicationController
   
   def create
-    @book = Book.new(book_params)
-    @book.user_id = current_user.id
-    if @book.save
+    @new_book = Book.new(book_params)
+    @new_book.user_id = current_user.id
+    if @new_book.save
      flash[:notice] = "You have created book successfully."
      redirect_to book_path(@book.id)
     else
-     flash.now[:notice] = "Create book error!"
-     render :index
+      @books = Book.all
+      render :index
     end
   end
+  
   def index
+    
     @new_book = Book.new
     @books = Book.all
   end
@@ -26,13 +28,12 @@ class BooksController < ApplicationController
   end
   
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
      flash[:notice] = "You have updated book successfully."
-     redirect_to book_path(book.id)
+     redirect_to book_path(@book.id)
     else
-     flash.now[:notice] = "Update book error!"
-     render :show
+      render :edit
     end
   end
   
